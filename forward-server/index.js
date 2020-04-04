29b2224f6bfa264a2ad4d74ff69fccb7ebcc7550
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-04-04 18:00:00 +0200

    feat: use a forward server to handle data transform and possibly fetching

---
+++ forward-server/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const csv = require("csvtojson");
+
+const app = express();
+const port = 9000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/geodata", async (req, res) => {
+  const { city = "gothenburg" } = req.body;
+  const data = await csv()
+    .fromFile(path.resolve(__dirname, `data/${city}.csv`))
+    .then((data) =>
+      data.map(({ weight, latitude, longitude }) => ({
+        weight: parseInt(weight),
+        latitude: parseFloat(latitude),
+        longitude: parseFloat(longitude),
+      }))
+    );
+
+  return res.send(data);
+});
+
+app.listen(port, () =>
+  console.log(`Forward server listening at http://localhost:${port}`)
+);
