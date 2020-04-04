5a0d8cb1b44187e87847b57ca9b1caf94c403944
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-04-04 18:44:19 +0200

    feat: show user position and stream location to forward-server

--- mobile-risk-zone/components/Map.js
+++ mobile-risk-zone/components/Map.js
@@ -1,23 +1,47 @@
 import React, { useEffect, useState } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { StyleSheet, View, Dimensions } from "react-native";
 import axios from "axios";
+import { useLocation } from "../hooks/useLocation";
 
 export function Map() {
   const [points, setPoints] = useState([]);
+  const [location, error] = useLocation({ watch: true });
+
   useEffect(() => {
+    if (!error) {
+      // send location to forward-server and from there to our backend
+      axios
+        .post("http://192.168.0.22:9000/report", {
+          data: JSON.stringify({ location }),
+        })
+        .catch(() => console.log("Failed to report"));
+    }
+  }, [location]);
+
+  useEffect(() => {
+    let source;
     async function loadData() {
+      source = axios.CancelToken.source();
       const { data } = await axios
         .post("http://192.168.0.22:9000/geodata", {
           data: JSON.stringify({ city: "gothenburg" }),
+          CancelToken: source.token,
         })
         .catch((e) => {
           console.log(e);
           return [];
         });
+
       setPoints(data);
     }
     loadData();
+
+    return () => {
+      if (source.cancel) {
+        source.cancel("Request cancelled");
+      }
+    };
   }, []);
 
   if (points.length === 0) {
@@ -29,12 +53,15 @@ export function Map() {
       <MapView
         style={styles.mapStyle}
         provider={PROVIDER_GOOGLE}
-        region={{
-          latitude: 57.7118511,
-          longitude: 11.9699815,
+        initialRegion={{
+          latitude: location?.coords?.latitude ?? 57.7118511,
+          longitude: location?.coords?.longitude ?? 11.9699815,
           latitudeDelta: 1,
           longitudeDelta: 1,
         }}
+        showsUserLocation={true}
+        minZoomLevel={10}
+        maxZoomLevel={20}
       >
         <MapView.Heatmap
           points={points}
